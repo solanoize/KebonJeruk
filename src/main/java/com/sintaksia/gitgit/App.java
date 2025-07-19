eaeee3084e37e389eea6187d2c7838bdbdc354c3
septian325 <septian325@gmail.com>
2025-07-19 17:49:32 +0700

    tambah nama Ivan

--- src/main/java/com/sintaksia/gitgit/App.java
+++ src/main/java/com/sintaksia/gitgit/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Ivan" );
+        System.out.println("Hello Ivan");
         System.out.println("Hello devina cantik");
         System.out.println("Hello World Fukato.");
         System.out.println("Hello World Cumi horee ini asik nih!");
