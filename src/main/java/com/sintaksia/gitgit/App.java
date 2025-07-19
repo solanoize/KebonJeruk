0dcac8ce5913f9805b871f157b01ffa8d1a2939d
Devina Vallery Theresia <devinavallery12@gmail.com>
2025-07-19 17:34:18 +0700

    updated

--- src/main/java/com/sintaksia/gitgit/App.java
+++ src/main/java/com/sintaksia/gitgit/App.java
@@ -4,14 +4,12 @@ package com.sintaksia.gitgit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
-        System.out.println( "Hello devina cantik" );
+        System.out.println("Hello devina cantik");
         System.out.println("Hello World Fukato.");
         System.out.println("Hello World Cumi horee ini asik nih!");
 
-}
+    }
 }
