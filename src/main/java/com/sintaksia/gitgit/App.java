726f7264b57e1cd6e9c919a26e5bf1b738b668ee
ne497 <ne49797@gmail.com>
2025-07-19 17:47:08 +0700

    updated

--- src/main/java/com/sintaksia/gitgit/App.java
+++ src/main/java/com/sintaksia/gitgit/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World Novaa!" );
+        System.out.println( "Heloww sistt Novaa!" );
         System.out.println("Hello World Fukato.");
         System.out.println("Hello World Cumi horee ini asik nih!");
     }
